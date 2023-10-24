4163c3ffe7f0b6ce690e527e8c8e5318154a2163
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2023-10-24 15:06:47 +0300

    end hw1

---
+++ task-2.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Задача 2. Доставка товару
+// Оголоси функцію getShippingMessage, яка очікує три параметри, значення яких будуть задаватися під час її виклику:
+
+// country — перший параметр, рядок, що містить країну доставки
+// price — другий параметр, число, що містить загальну вартість товару
+// deliveryFee — третій параметр, число, що містить вартість доставки товару
+// Доповни код функції так, щоб вона повертала рядок з повідомленням про доставку товару в країну користувача: "Shipping to <country> will cost <totalPrice> credits", де:
+
+// <country> — це країни доставки
+// <totalPrice> — це загальна вартість замовлення, що включає вартість товару і його доставки
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+function getShippingMessage(country, price, deliveryFee) {
+	return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+}
+
+console.log(getShippingMessage('Australia', 120, 50));
+console.log(getShippingMessage('Germany', 80, 20));
+console.log(getShippingMessage('Sweden', 100, 20));
